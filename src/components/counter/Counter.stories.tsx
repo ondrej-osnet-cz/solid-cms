675642e2b5ae8f9976f974bcf17f37822da12bb0
Ondřej Wagenknecht <ondrej@osnet.cz>
2023-03-11 17:20:50 +0100

    fix count event

--- src/components/counter/Counter.stories.tsx
+++ src/components/counter/Counter.stories.tsx
@@ -1,6 +1,6 @@
-import Counter from "./Counter";
-import type { Meta, StoryFn } from "@storybook/html";
-import type { ComponentProps } from "solid-js";
+import type { Meta, StoryFn } from '@storybook/html';
+import type { ComponentProps } from 'solid-js';
+import Counter from './Counter';
 
 // example with Template
 const Template = ((args) => <Counter {...args} />) as StoryFn<ComponentProps<typeof Counter>>;
@@ -9,18 +9,17 @@ export const SimpleCounter = Template.bind({});
 
 export const CounterWithInit = Template.bind({});
 CounterWithInit.args = {
-  initialValue: 10,
+  initialValue: 10
 };
-
-
 
 export default {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/html/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Counter",
+  title: 'Counter',
   argTypes: {
-    initialValue: { control: "number" },
-  },
+    initialValue: { control: 'number' },
+    onCounterChange: { action: 'onCounterChange' }
+  }
 } as Meta<ComponentProps<typeof Counter>>;
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,12 +2,19 @@ import { createSignal } from 'solid-js';
 
 export interface CounterProps {
   initialValue?: number;
+  onCounterChange?(count: number): void;
 }
 
 export default function Counter(props: CounterProps) {
   const [count, setCount] = createSignal(props.initialValue || 0);
+
+  const handleCounterChange = () => {
+    setCount(count() + 1);
+    props.onCounterChange?.(count());
+  };
+
   return (
-    <div class="bg-slate-600 text-slate-300 rounded-md cursor-pointer p-3 w-fit" onClick={() => setCount(count() + 1)}>
+    <div class="bg-slate-600 text-slate-300 rounded-md cursor-pointer p-3 w-fit" onClick={handleCounterChange}>
       Clicks: {count()}
     </div>
   );
