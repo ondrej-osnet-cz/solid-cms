66b4c3abafc41be180ebe9e0aa74a00cca8c1f89
Ondřej Wagenknecht <ondrej@osnet.cz>
2023-04-08 07:05:35 +0200

    fix load data

--- src/libs/server/database/database-connection.ts
+++ src/libs/server/database/database-connection.ts
@@ -5,6 +5,7 @@ export async function connectToDatabase(): Promise<typeof mongoose | undefined> 
   try {
     const mongooseConnected = await mongoose.connect(mongoConfig.mongoUri);
     console.log('Connected to database');
+
     return mongooseConnected;
   } catch (error) {
     console.log('Error connecting to database', error);
--- src/routes/(default).tsx
+++ src/routes/(default).tsx
@@ -1,21 +1,36 @@
-import { For, createResource, onMount } from 'solid-js';
-import { A, Outlet, useLocation, useRouteData } from 'solid-start';
+import { For, onMount } from 'solid-js';
+import { isServer } from 'solid-js/web';
+import { A, Outlet, createRouteData, useLocation, useRouteData } from 'solid-start';
+import { createServerData$ } from 'solid-start/server';
 import { Menu } from '~/libs/core/bo/menu';
+import MenuModel from '~/libs/server/database/models/menu/menu.model';
 
 export function routeData() {
-  const [menu] = createResource(async () => {
-    const response = await fetch('http://localhost:3000/api/v1/menu');
-    return (await response.json()) as Menu;
-  });
+  let menu;
+  if (isServer) {
+    menu = createServerData$(async () => {
+      const menuModel = await MenuModel.findOne();
+      const menuJson = JSON.parse(JSON.stringify(menuModel?.toJSON())) as Menu;
+      console.log('server menu', menuJson);
+      return menuJson;
+    });
+  } else {
+    menu = createRouteData(async () => {
+      const response = await fetch('http://localhost:3000/api/v1/menu');
+      const menu = (await response.json()) as Menu;
+      console.log('client menu', menu);
+      return menu;
+    });
+  }
   return { menu };
 }
 
 export default function Layout() {
   console.log('Layout');
-  const { menu } = useRouteData<typeof routeData>();
+  const data = useRouteData<typeof routeData>();
 
   onMount(() => {
-    console.log('Layout onMount', menu());
+    console.log('Layout onMount', data.menu());
   });
 
   const active = (path: string) => {
@@ -33,7 +48,7 @@ export default function Layout() {
     <>
       <nav class="bg-sky-800">
         <ul class="container flex items-center p-3 text-gray-200">
-          <For each={menu()?.items} fallback={<div>Loading...</div>}>
+          <For each={data.menu()?.items} fallback={<div>Loading...</div>}>
             {(item) => (
               <li onClick={() => handleItemClick(item.url)} class={`border-b-2 ${active(item.url)} mx-1.5 sm:mx-6`}>
                 <A href={item.url}>{item.label}</A>
